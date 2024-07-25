a03a0b4973ec0f6c174cf622e9028824293305cf
Samarth Joshi <127830200+samarthjoshi2512@users.noreply.github.com>
2024-07-25 19:25:27 +0530

    Update Weather.py

--- WeatherApp/Weather.py
+++ WeatherApp/Weather.py
@@ -1,7 +1,7 @@
 import requests
 
 def forecastWeather(lat,lon):
-    weatherurl=f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=f7be77490ec3ed1602ae0654b36efe27'
+    weatherurl=f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=ADD YOUR API KEY'
     weatherData=requests.get(weatherurl).json()
     weatherData={
         "temperature":weatherData['main']['temp'],
